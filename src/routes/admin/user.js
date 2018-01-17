c5aa835fe0a9ace56e20d8b20745049d8e8e1d71
Jorge Lopez <jorge@allcodingsolutions.com>
2018-01-16 21:26:00 -0500

    Adding missing changes from merge

--- src/routes/admin/user.js
+++ src/routes/admin/user.js
@@ -28,5 +28,8 @@ router.post('/delete/:id', control.postDeleteUser);
 router.post('/activation/:id', control.activationUser);
 router.get('/resetPassword/:id', control.getPasswordReset);
 router.post('/resetPassword/:id', control.postPasswordReset);
+router.get('/card/:id', control.getBingoCard);
+router.get('/resetCard/:id', control.getCardReset);
+router.post('/resetCard/:id', control.postCardReset);
 
 module.exports = router;
