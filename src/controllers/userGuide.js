c7e0a70debff6886e9d427606c7da9ff058b54ae
Erik the Red <Khaosklub@gmail.com>
2018-01-29 09:36:09 -0500

    initial commit of BOLO 10.0
    Project is as I recieved it from pinecrest PD

---
+++ src/controllers/userGuide.js
@@ -0,0 +1,42 @@
+/**
+ * This class sets the controls for the User Guide routes
+ */
+
+var fs = require('fs');
+var md = require("node-markdown").Markdown;
+
+/**
+ * Displays the user guide for each individual tier.
+ */
+exports.getUserGuide = function(req, res, next) {
+
+  var file = '';
+
+  if (res.locals.userTier == "ROOT")
+    file = appRoot + '/public/UserGuide/Root.md';
+  if (res.locals.userTier == "SUPERVISOR")
+    file = appRoot + '/public/UserGuide/Supervisor.md';
+  if (res.locals.userTier == "ADMINISTRATOR")
+    file = appRoot + '/public/UserGuide/Administrator.md';
+  if (res.locals.userTier == "OFFICER")
+    file = appRoot + '/public/UserGuide/Officer.md';
+
+  var grid = req.session.grid;
+  if(grid)
+  {
+  fs.readFile(file, function(err, data) {
+    if (err)
+      req.flash('error_msg', 'User Guide could not be found!');
+    else {
+      res.render('user-guide', {
+        md: md,
+        text: data.toString()
+      });
+    }
+  });
+  }
+  else
+  {
+	  res.redirect('/bingo');
+  }
+};
